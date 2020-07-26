b261d5a19b94d005b36e35a7a0e685ae69bcaefd
DriveDownHigh <62028515+DriveDownHigh@users.noreply.github.com>
2020-07-26 10:44:45 +0200

    Add files via upload

--- 02_custom_functions.js
+++ 02_custom_functions.js
@@ -437,7 +437,7 @@ const key_press_practice_hrf = function (config, CT, magpie, answer_container_ge
             if (keyPressed === config.data[CT].key1 || keyPressed === config.data[CT].key2) {
                 let correctness;
                 const RT = Date.now() - startingTime; // measure RT before anything else
-                console.log(config.data[CT].expected + " " + config.data[CT][keyPressed.toUpperCase());
+                console.log(config.data[CT].expected + " " + config.data[CT][keyPressed.toUpperCase()]);
                 if (
                     config.data[CT].expected ===
                     config.data[CT][keyPressed.toUpperCase()]
