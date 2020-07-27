6ef5ae263331e74223a1dcb4417398c8e0959e16
DriveDownHigh <62028515+DriveDownHigh@users.noreply.github.com>
2020-07-27 16:25:22 +0200

    Update 02_custom_functions.js

--- 02_custom_functions.js
+++ 02_custom_functions.js
@@ -201,6 +201,7 @@ const key_press_hrf = function (config, CT, magpie, answer_container_generator, 
             if (e.which === 32 && spaceCounter === sentence3.length && bool_knowledge && bool_trigger && bool_continuation) {
                 $(".question-container").append(answer_container_generator(config, CT));
                 $(".magpie-response-keypress-header").removeClass("hidden");
+                $(".magpie-view-question").addClass("hidden");
                 bool_question = true;
             }
         } else if(e.which === 78 && bool_question || e.which === 89 && bool_question) {
